ee76f5a53c77eb18598a8cec081fc9eaa4ad6f4b
Tandara Petrisor-Alexandru <tandara.petrisor@outlook.com>
2022-01-08 15:25:22 +0200

    Added DbContext in the services

--- ePlants/Startup.cs
+++ ePlants/Startup.cs
@@ -1,3 +1,4 @@
+using ePlants.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -23,6 +24,9 @@ namespace ePlants
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //DbContext config
+            services.AddDbContext<PlantsDbContext>();
+
             services.AddControllersWithViews();
         }
 
