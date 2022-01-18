bbddce1ac836cd18cbd0009d3f7112c7e5d943a2
petrisoralexandru <kwixacademy@gmail.com>
2022-01-18 14:56:32 +0200

    Folder structure for Dependency Injection (Services)

---
+++ ePlants/Data/Services/IManufacturersService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ePlants.Models;
+
+namespace ePlants.Data.Services
+{
+    public interface IManufacturersService
+    {
+        // CRUD Manufacturers
+
+        // add manufacturers
+        void Add(ManufacturerModel manufacturer);
+        // get all manufacturers
+        IEnumerable<ManufacturerModel> GetAll();
+        // return a single manufacturer
+        ManufacturerModel GetById(int id);
+        // check if the id exists before updating a manufacturer
+        ManufacturerModel Update(int id, ManufacturerModel manufacturer);
+        // delete a manufacturer
+        void Delete(int id);
+        
+    }
+}
---
+++ ePlants/Data/Services/ManufacturersService.cs
@@ -0,0 +1,42 @@
+﻿using ePlants.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ePlants.Data.Services
+{
+    public class ManufacturersService : IManufacturersService
+    {
+        private readonly PlantsDbContext _context;
+
+        public ManufacturersService(PlantsDbContext _context)
+        {
+            this._context = _context;
+        }
+
+        public void Add(ManufacturerModel manufacturer)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<ManufacturerModel> GetAll()
+        {
+            IEnumerable<ManufacturerModel> result = _context.Manufacturers.ToList();
+            return result;
+        }
+
+        public ManufacturerModel GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ManufacturerModel Update(int id, ManufacturerModel manufacturer)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
